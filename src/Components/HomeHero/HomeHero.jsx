31b23c5f6a83192afc457f3fc23206b621f04e95
Diego Matera <diegomatera@gmail.com>
2023-05-19 00:09:19 -0300

    Animaciones hero y navbar desktop

--- src/Components/HomeHero/HomeHero.jsx
+++ src/Components/HomeHero/HomeHero.jsx
@@ -1,4 +1,6 @@
-import HeroImage from "../../Assets/Images/home-hero-bg.webp";
+import HeroImageMobile from "../../Assets/Images/home-hero-img-mobile.webp";
+import HeroImageDesktop from "../../Assets/Images/home-hero-img-desktop.webp";
+
 import HeroIg from "../../Assets/Images/media-ig-grey.svg";
 import HeroBe from "../../Assets/Images/media-be-grey.svg";
 import HeroIn from "../../Assets/Images/media-in-grey.svg";
@@ -12,28 +14,62 @@ export default function HomeHero() {
             href="www.instagram.com"
             className="HomeHeroContainer__Media--Link"
           >
-            <img alt="Instagram" className="Hero-Ig" src={HeroIg}></img>
+            <span className="Hero-Media-Animation Media-Animation"></span>
+            <img
+              alt="Instagram"
+              className="Hero-Ig Hero-Media-Image"
+              src={HeroIg}
+            ></img>
           </a>
           <a href="www.behance.com" className="HomeHeroContainer__Media--Link">
-            <img alt="Behance" className="Hero-Be" src={HeroBe}></img>
+            <span className="Hero-Media-Animation Media-Animation"></span>
+
+            <img
+              alt="Behance"
+              className="Hero-Be Hero-Media-Image"
+              src={HeroBe}
+            ></img>
           </a>
           <a href="www.linkedin.com" className="HomeHeroContainer__Media--Link">
-            <img alt="Linkedin" className="Hero-In" src={HeroIn}></img>
+            <span className="Hero-Media-Animation Media-Animation"></span>
+
+            <img
+              alt="Linkedin"
+              className="Hero-In Hero-Media-Image"
+              src={HeroIn}
+            ></img>
           </a>
         </div>
-        <p className="HomeHeroContainer__Title">
-          Transformamos ideas en marcas con personalidad
+        <p className="HomeHeroContainer__Title Mobile">
+          Transformamos tu marca en <br /> una identidad inolvidable.
+        </p>
+        <p className="HomeHeroContainer__Title HeroTitle1 Desktop">
+          {" "}
+          <span></span>Transformamos tu
+        </p>
+        <p className="HomeHeroContainer__Title HeroTitle2 Desktop">
+          <span></span> marca en una identidad
+        </p>
+        <p className="HomeHeroContainer__Title HeroTitle3 Desktop">
+          <span></span>inolvidable.
         </p>
         <p className="HomeHeroContainer__Subtitle">
           <span>Estudio boutique creativo</span>Branding. UX. Social media &
           más.
         </p>
         <div className="HomeHeroContainer__ImageContainer">
-          <img
-            alt="Background"
-            src={HeroImage}
-            className="HomeHeroContainer__ImageContainer--Image"
-          ></img>
+          <div className="HomeHeroContainer__ImageContainer--Image">
+            <img
+              alt="Background"
+              src={HeroImageMobile}
+              className="Mobile Hero-Image"
+            ></img>
+            <img
+              alt="Background"
+              src={HeroImageDesktop}
+              className="Desktop Hero-Image"
+            ></img>
+          </div>
         </div>
         <div className="HomeHeroContainer__Block1"></div>
         <div className="HomeHeroContainer__Block2"></div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logoVernal from "../../Assets/Images/logo-vernal-white.svg";
 import logoMenu from "../../Assets/Images/logo-vernal-black.png";
+import closeMenu from "../../Assets/Images/menu-close.png";
 import languageArrowD from "../../Assets/Images/arrow.svg";
 import languageArrowW from "../../Assets/Images/arrow-white.svg";
 import Ig from "../../Assets/Images/media-ig-white.svg";
@@ -12,10 +13,12 @@ import MenuButton from "../../Assets/Images/menu-button.svg";
 
 export default function NavBar() {
   const [menu, setMenu] = useState(false);
+  const [zIndex, setZIndex] = useState(false);
   const [menuButton, setMenuButton] = useState(false);
   const [menuIsActive, setMenuIsActive] = useState(false);
   const toggleMenu = () => {
     setMenu(!menu);
+    setZIndex(true);
     setMenuButton(!menuButton);
     setMenuIsActive(!menuIsActive);
   };
@@ -53,17 +56,29 @@ export default function NavBar() {
         </div>
       </div>
       {/* MENU */}
-      <div className={`Menu ${menu ? "MenuIsActive" : ""}`}>
+      <div
+        style={{ zIndex }}
+        id={`${zIndex ? "MenuContainer" : "MenuContainerZIndex"}`}
+        className={`Menu ${menu ? "MenuIsActive" : ""}`}
+      >
         <div className="MenuContainer">
           <div className="MenuContainer__Top">
             {/* CLOSE BUTTON */}
             <div
-              className="MenuContainer__Top--CloseButton"
+              className={`MenuContainer__Top--CloseButton ${
+                menu
+                  ? "Navbar-ButtonIcon-AnimationIn"
+                  : "Navbar-ButtonIcon-AnimationOut"
+              }`}
               onClick={toggleMenu}
-            ></div>
+            >
+              <img alt="Close" className="CloseButton" src={closeMenu}></img>
+            </div>
             {/* LOGO */}
             <img
-              className="MenuContainer__Top--Logo"
+              className={`MenuContainer__Top--Logo ${
+                menu ? "Navbar-Logo-AnimationIn" : "Navbar-Logo-AnimationOut"
+              }`}
               alt="Logo"
               src={logoMenu}
             ></img>
@@ -74,40 +89,106 @@ export default function NavBar() {
           </div>
           {/* MENU OPTIONS */}
           <div className="MenuContainer__Links">
-            <a href="#Nosotros" className="MenuContainer__Links--Options">
+            <a
+              href="#Nosotros"
+              className={`MenuContainer__Links--Options  ${
+                menu ? "Navbar-Links-AnimationIn" : "Navbar-Links-AnimationOut"
+              }`}
+            >
               Nosotros
             </a>
-            <a href="#Nosotros" className="MenuContainer__Links--Options">
+            <a
+              href="#Nosotros"
+              className={`MenuContainer__Links--Options   ${
+                menu ? "Navbar-Links-AnimationIn" : "Navbar-Links-AnimationOut"
+              }`}
+            >
               Proyectos
             </a>
-            <a href="#Nosotros" className="MenuContainer__Links--Options">
+            <a
+              href="#Nosotros"
+              className={`MenuContainer__Links--Options   ${
+                menu ? "Navbar-Links-AnimationIn" : "Navbar-Links-AnimationOut"
+              }`}
+            >
               Servicios
             </a>
-            <a href="#Nosotros" className="MenuContainer__Links--Options">
+            <a
+              href="#Nosotros"
+              className={`MenuContainer__Links--Options   ${
+                menu ? "Navbar-Links-AnimationIn" : "Navbar-Links-AnimationOut"
+              }`}
+            >
               Metodología
             </a>
           </div>
           <div className="MenuContainer__Media">
             <a className="MenuContainer__Media--Link" href="www.instagram.com">
               {" "}
-              <img alt="Instagram" src={Ig} className="Menu-Ig"></img>
+              <img
+                alt="Instagram"
+                src={Ig}
+                className={`Menu-Ig ${
+                  menu
+                    ? "Navbar-MediaLinks-AnimationIn"
+                    : "Navbar-MediaLinks-AnimationOut"
+                }`}
+              ></img>
             </a>
             <a className="MenuContainer__Media--Link" href="www.behance.com">
               {" "}
-              <img alt="Behance" src={Be} className="Menu-Be"></img>
+              <img
+                alt="Behance"
+                src={Be}
+                className={`Menu-Be ${
+                  menu
+                    ? "Navbar-MediaLinks-AnimationIn"
+                    : "Navbar-MediaLinks-AnimationOut"
+                }`}
+              ></img>
             </a>
             <a className="MenuContainer__Media--Link" href="www.linkedin.com">
               {" "}
-              <img alt="Linkedin" src={In} className="Menu-In"></img>
+              <img
+                alt="Linkedin"
+                src={In}
+                className={`Menu-In ${
+                  menu
+                    ? "Navbar-MediaLinks-AnimationIn"
+                    : "Navbar-MediaLinks-AnimationOut"
+                }`}
+              ></img>
             </a>
             <a className="MenuContainer__Media--Link" href="www.whatsapp.com">
               {" "}
-              <img alt="WhatsApp" src={Wsp} className="Menu-Wsp"></img>
+              <img
+                alt="WhatsApp"
+                src={Wsp}
+                className={`Menu-Wsp ${
+                  menu
+                    ? "Navbar-MediaLinks-AnimationIn"
+                    : "Navbar-MediaLinks-AnimationOut"
+                }`}
+              ></img>
             </a>
           </div>
           <div className="MenuContainer__Contact">
-            <div className="MenuContainer__Contact--Arrow"></div>
-            <p className="MenuContainer__Contact--Text">ESCRIBINOS</p>
+            <div
+              className={`MenuContainer__Contact--Arrow ${
+                menu
+                  ? "Navbar-ButtonIcon-AnimationIn"
+                  : "Navbar-ButtonIcon-AnimationOut"
+              }`}
+            ></div>
+            <p
+              className={`MenuContainer__Contact--Text ${
+                menu
+                  ? "Navbar-ButtonText-AnimationIn"
+                  : "Navbar-ButtonText-AnimationOut"
+              }`}
+            >
+              ESCRIBINOS
+            </p>
           </div>
         </div>
       </div>
