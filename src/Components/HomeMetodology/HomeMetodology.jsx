21bc4c8c9c7683af5a9919a509615abd4a738b0c
Diego Matera <diegomatera@gmail.com>
2023-05-26 19:14:09 -0300

    Ajuste seccion approach

--- src/Components/HomeMetodology/HomeMetodology.jsx
+++ src/Components/HomeMetodology/HomeMetodology.jsx
@@ -5,44 +5,62 @@ export default function HomeMetodology() {
     <div className="HomeMetodology">
       <div className="HomeMetodologyContainer">
         <p className="HomeMetodologyContainer__SectionTitle SectionTitle">
-          Metodología <img src={TitleLine} alt="Icon"></img>
+          Approach <img src={TitleLine} alt="Icon"></img>
         </p>
         {/* SUBCONTAINER */}
         <div className="HomeMetodologyContainer__Subcontainer">
           <p className="HomeMetodologyContainer__Subcontainer--Number">01</p>
-          <p className="HomeMetodologyContainer__Subcontainer--Title">
-            Empatizamos
+          <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
+
+          <p className="HomeMetodologyContainer__Subcontainer--Title Title">
+            Conexión
           </p>
-          <div className="HomeMetodologyContainer__Subcontainer--Button"></div>
+          <p className="HomeMetodologyContainer__Subcontainer--Text Text">
+            <span>El primer paso es conocernos.</span>Aprendemos acerca de tus
+            objetivos y expectativas para crear un plan de trabajo
+            personalizado.
+          </p>
         </div>
-        <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
         {/* SUBCONTAINER */}
         <div className="HomeMetodologyContainer__Subcontainer">
           <p className="HomeMetodologyContainer__Subcontainer--Number">02</p>
-          <p className="HomeMetodologyContainer__Subcontainer--Title">
-            Investigamos
-          </p>
-          <div className="HomeMetodologyContainer__Subcontainer--Button"></div>
-        </div>
-        <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
+          <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
 
+          <p className="HomeMetodologyContainer__Subcontainer--Title Title">
+            Perspectiva
+          </p>
+          <p className="HomeMetodologyContainer__Subcontainer--Text Text">
+            <span>Un plan de impacto requiere perspectiva.</span>Investigamos
+            sobre tus consumidores, tendencias relevantes, competidores, el
+            mercado y más.
+          </p>
+        </div>
         {/* SUBCONTAINER */}
         <div className="HomeMetodologyContainer__Subcontainer">
           <p className="HomeMetodologyContainer__Subcontainer--Number">03</p>
-          <p className="HomeMetodologyContainer__Subcontainer--Title">
-            Conceptualizamos
-          </p>
-          <div className="HomeMetodologyContainer__Subcontainer--Button"></div>
-        </div>
-        <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
+          <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
 
+          <p className="HomeMetodologyContainer__Subcontainer--Title Title">
+            Concepto
+          </p>
+          <p className="HomeMetodologyContainer__Subcontainer--Text Text">
+            <span>Creamos un concepto poderoso</span>Fusinamos creatividad y
+            estrategia para generar ideas efectivas que reflejen la escencia de
+            la marca.
+          </p>
+        </div>
         {/* SUBCONTAINER */}
         <div className="HomeMetodologyContainer__Subcontainer">
           <p className="HomeMetodologyContainer__Subcontainer--Number">04</p>
-          <p className="HomeMetodologyContainer__Subcontainer--Title">
-            Creamos
+          <div className="HomeMetodologyContainer__Subcontainer--Line"></div>
+
+          <p className="HomeMetodologyContainer__Subcontainer--Title Title">
+            Construcción
           </p>
-          <div className="HomeMetodologyContainer__Subcontainer--Button"></div>
+          <p className="HomeMetodologyContainer__Subcontainer--Text Text">
+            <span>Una experiencia de marca integral.</span>Cada elemento de
+            comunicación es diseñado para lograr un resultado cohesivo y único.
+          </p>
         </div>
       </div>
     </div>
